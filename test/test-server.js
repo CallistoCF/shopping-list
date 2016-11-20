f538375f1c3e892bee6eb0789485fe7e78715cbe
CallistoCF <chrisfrater@gmail.com>
2016-11-19 19:04:42 -0800

    fifth commit, mocha 1

--- test/test-server.js
+++ test/test-server.js
@@ -54,9 +54,33 @@ describe('Shopping List', function() {
                 done();
             });
     });
-    it('should edit an item on put');
-    it('should delete an item on delete');
-    it('should POST to an ID that doesn\'t exist');
+    it('should edit an item on PUT');
+    it('should delete an item on DELETE');
+    it('should POST to an ID that doesn\'t exist', function(done) {
+        chai.request(app)
+            .post('/items')
+            .send({'name': 'Kale'})
+            .end(function(err, res) {
+                should.equal(err, null);
+                res.should.have.status(201);
+                res.should.be.json;
+                res.body.should.be.a('object');
+                res.body.should.have.property('name');
+                res.body.should.not.have.property('id');
+                res.body.name.should.be.a('string');
+                //res.body.id.should.not.be.a('number');
+                res.body.name.should.equal('Kale');
+                storage.items.should.be.a('array');
+                storage.items.should.have.length(4);
+                storage.items[3].should.be.a('object');
+                storage.items[3].should.have.property('id');
+                storage.items[3].should.have.property('name');
+                storage.items[3].id.should.be.a('number');
+                storage.items[3].name.should.be.a('string');
+                storage.items[3].name.should.equal('Kale');
+                done();
+            });
+    }););
     it('should POST without body data');
     it('should POST with something other than valid JSON');
     it('should PUT without an ID in the endpoint');
